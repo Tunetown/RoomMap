f0c617043ef130433acff9dc3266200d167b429d
tweber <tom-vibrant@gmx.de>
2017-01-03 16:36:35 +0100

    .

--- src/de/tunetown/roommap/view/data/OutputGraphics.java
+++ src/de/tunetown/roommap/view/data/OutputGraphics.java
@@ -9,9 +9,7 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
 import java.util.concurrent.TimeUnit;
-
 import javax.swing.JPanel;
-
 import rainbowvis.Rainbow;
 import de.tunetown.roommap.main.Main;
 import de.tunetown.roommap.model.Measurement;;
